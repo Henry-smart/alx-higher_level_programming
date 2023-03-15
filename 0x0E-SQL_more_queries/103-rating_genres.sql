734b7b6bb2a6e9c9cb6a152f6b33f756171b4c13
henrysmart <henryeziechina2@gmail.com>
2023-03-15 23:49:50 +0100

    bug_fix

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,6 @@
--- Lists all genres from hbtn_0d_tvshows and displays
--- the number of shows linked to each.
-SELECT `tv_genres`.`name` AS `genre`,
-	COUNT(`tv_show_genres`.`genre_id`) AS 'number_of_shows'
-	FROM `tv_genres` RIGHT JOIN `tv_show_genres`
-	ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
-	GROUP BY `genre`
-	ORDER BY 'number_of_show' DESC;
+-- Lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+SELECT `name`, SUM(`tv_show_ratings`.`rate`) 'rating' FROM `tv_genres`
+	INNER JOIN `tv_show_genres` ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
+	INNER JOIN `tv_show_ratings` ON `tv_show_genres`.`show_id` = `tv_show_ratings`.`show_id`
+	GROUP BY `name`
+	ORDER BY `rating` DESC;
